e966b8aa55721194665126f9d284763f9ec7d9e6
Francois Rousseau <frodjango@Francoiss-MacBook-Pro-2.local>
2019-01-10 10:24:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+/*
+January 2019
+
+The actual data is stored in a fix variable (instead of an API call that would normally provide this data),
+*/
+
+import React from "react"
+
+// Rem: importing from an external folder (outside of src) does not seems to work.
+
+import TodoItem from "./TodoItemProject/TodoItem.js"
+import todosData from "./TodoItemProject/todosData.js"
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+        /* {
+            id: 1,
+            text: "Take out the trash",
+            completed: true
+        } */
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)    // trick suggested by the author
+    }
+
+    handleChange(id) {
+        // This in fact executed from the TodoItem function component who adds an argument from
+        // the props (the actual key that is the id).
+        //
+        // This id will serve here as an identifier of todo item.
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+    render() {
+        const todoItems = this.state.todos.map(
+            item => <TodoItem
+            key={item.id}
+            item={item}
+            handleChange={this.handleChange}
+            />)
+
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
+}
+
+export default App
