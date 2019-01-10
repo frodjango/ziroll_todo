24c3df65e39a524b76dade150967a228cfad7fbf
Francois Rousseau <frodjango@Francoiss-MacBook-Pro-2.local>
2019-01-10 11:29:22 -0500

    Adding comments and refactoring a bit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,39 @@
 January 2019
 
 The actual data is stored in a fix variable (instead of an API call that would normally provide this data),
+
+V1
+
+Our application is only one logical component - a list of to dos...
+
+V2
+
+To reflect a more realistic web app we will consider that the list of to do is an actual visual component within
+the App.
+
+Let's refactor...done
+
+Properties hierarchy
+
+App
+    ToDoList (has state)
+        TodoItem (has no state - gets its prop from the above layer)
+
+Styling
+
+    App has no styling of itw own
+    ToDoList has its own css class WITHOUT conditional styling
+    TodoItems has itw own styling AND conditional styling to reflect state
+
+Action propagation
+
+    App has no action
+    ToDoList has the overall state of the application so it needs to be told
+        when the user chages the state of one of its items. Actual handling is
+        executed at the following layer (TodoItems) -  a call back is passed within
+        the JSX (using props of course).
+    TodoItems handles the action by executing the code of the callback (in props)
+
 */
 
 import React from "react"
@@ -11,7 +44,15 @@ import React from "react"
 import TodoItem from "./TodoItemProject/TodoItem.js"
 import todosData from "./TodoItemProject/todosData.js"
 
+
 class App extends React.Component {
+
+    render() {
+        return < ToDoList />
+    }
+}
+
+class ToDoList extends React.Component {
 
     constructor() {
         super()
@@ -20,6 +61,9 @@ class App extends React.Component {
             text: "Take out the trash",
             completed: true
         } */
+
+        // This 'emulates' a real worldd API call that would return a certain quantity of to do items.
+
         this.state = {
             todos: todosData
         }
@@ -27,31 +71,66 @@ class App extends React.Component {
     }
 
     handleChange(id) {
+
         // This in fact executed from the TodoItem function component who adds an argument from
         // the props (the actual key that is the id).
         //
         // This id will serve here as an identifier of todo item.
+
+        // Remarque on the code below:
+        //
+        // We're looping around all data in hope to match the id.
+        // if found then toggle the 'completed' field, otherwise just return the item.
+
+
+        // a setState like this one MUST return the new updated "state" (in fact a new copy)
+        
         this.setState(prevState => {
+
+            // Phase 1
+
             const updatedTodos = prevState.todos.map(todo => {
                 if (todo.id === id) {
-                    todo.completed = !todo.completed
+                    todo.completed = !todo.completed        // actual work - this line !!!
                 }
                 return todo
             })
+            
+            // Phase 2
+
             return {
                 todos: updatedTodos
             }
         })
     }
+
     render() {
+
+        // map will process an array of objects and for each entry (item here) will
+        // generate a new entry...this is like saying
+        // new array = processed argument array
+        //  or
+        // todoItems = this.state.todos.map(recipe)
+        //
+        // the recipe here is a formatting one from raw data (id and text and completion flag)
+        // to JSX data
+        //
+
         const todoItems = this.state.todos.map(
             item => <TodoItem
-            key={item.id}
-            item={item}
-            handleChange={this.handleChange}
-            />)
-
+                        key={item.id}
+                        item={item}
+                        handleChange={this.handleChange}
+                    />)
+    
         return (
+
+            // 1- formating style is set explicitly here for the whole
+            // list (the todo-list class - see style.css)
+            // 2- The display array of 'TodoItem' is not limited or filter - raw output, a big list...
+
+            // JSX
+
             <div className="todo-list">
                 {todoItems}
             </div>
--- src/TodoItemProject/TodoItem.js
+++ src/TodoItemProject/TodoItem.js
@@ -2,18 +2,28 @@ import React from "react"
 
 
 function TodoItem(props) {
+
+    // Conditional styling:
+    // The style for a completed to do
+
     const completedStyle = {
         fontStyle: "italic",
         color: "#cdcdcd",
         textDecoration: "line-through"
     }
+
+    // JSX 
+
     return (
+        // two elements will be shown: INPUT & PARAGRAPH
         <div className="todo-item">
+
             <input 
                 type="checkbox" 
                 checked={props.item.completed} 
                 onChange={() => props.handleChange(props.item.id)}
             />
+
             <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
 
         </div>
